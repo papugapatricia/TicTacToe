d3939a6ef2ad06a5d8d0da7391526b3cc24cc61a
papugapatricia <patriciapapuga67@gmail.com>
2024-10-22 21:33:42 +0300

    Update cell.cpp

--- cell.cpp
+++ cell.cpp
@@ -1,12 +1,9 @@
 #include "cell.hpp"
 
-// Constructor cu parametri
 Cell::Cell(int r, int c, CellState s) : row(r), col(c), state(s) {}
 
-// Constructor de copiere
 Cell::Cell(const Cell& other) : row(other.row), col(other.col), state(other.state) {}
 
-// Operator de atribuire
 Cell& Cell::operator=(const Cell& other) {
     if (this != &other) {
         row = other.row;
@@ -16,18 +13,14 @@ Cell& Cell::operator=(const Cell& other) {
     return *this;
 }
 
-// Operator de egalitate
 bool Cell::operator==(const Cell& other) const {
     return row == other.row && col == other.col && state == other.state;
 }
 
-// GetState
 CellState Cell::GetState() const { return state; }
 
-// SetState
 void Cell::SetState(CellState s) { state = s; }
 
-// Operator de intrare
 std::istream& operator>>(std::istream& in, Cell& cell) {
     int r, c, s;
     in >> r >> c >> s;
@@ -35,7 +28,6 @@ std::istream& operator>>(std::istream& in, Cell& cell) {
     return in;
 }
 
-// Operator de ieșire
 std::ostream& operator<<(std::ostream& out, const Cell& cell) {
     out << "Row: " << cell.row << ", Col: " << cell.col << ", State: " << static_cast<int>(cell.state);
     return out;
