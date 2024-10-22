e0e122ab4cbcc1b8dee1e115879d85bd031390d0
papugapatricia <patriciapapuga67@gmail.com>
2024-10-22 21:33:16 +0300

    Update board.hpp

--- board.hpp
+++ board.hpp
@@ -4,30 +4,24 @@
 #include <iostream>
 
 class Board {
-    Cell _cells[3][3]; // Tabla de joc TicTacToe 3x3
-    int size;  // Dimensiunea tablei de joc (3x3 implicit)
+    Cell _cells[3][3]; 
+    int size; 
 
 public:
-    // Constructori
-    Board();  // Constructor implicit
-    Board(int size);  // Constructor cu parametri
-    Board(const Board& other);  // Constructor de copiere
+    Board();  
+    Board(int size);  
+    Board(const Board& other);  
 
-    // Operator de atribuire
     Board& operator=(const Board& other);
 
-    // Operator de egalitate
     bool operator==(const Board& other) const;
 
-    // Setteri și getteri pentru celule
     void SetCell(int row, int col, CellState state);
     CellState GetCell(int row, int col) const;
 
-    // Verificare stare joc
     bool IsFull() const;
     bool CheckWin(CellState state) const;
 
-    // Operatorii de intrare/ieșire
     friend std::istream& operator>>(std::istream& in, Board& board);
     friend std::ostream& operator<<(std::ostream& out, const Board& board);
 };
