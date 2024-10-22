55bb422e8e25196e1fb52ae62183dcc11c9e8ef6
papugapatricia <patriciapapuga67@gmail.com>
2024-10-22 21:37:34 +0300

    Update painter.cpp

--- painter.cpp
+++ painter.cpp
@@ -1,20 +1,15 @@
 #include "player.hpp"
 
-// Constructor implicit
 Player::Player(CellState symbol) : _symbol(symbol) {}
 
-// Constructor de copiere
 Player::Player(const Player& other) : _symbol(other._symbol) {}
 
 Player& Player::operator=(const Player& other) {
-    if (this != &other) { // Check for self-assignment
+    if (this != &other) { 
         _symbol = other._symbol;
     }
-    return *this; // Return a reference to the current object
+    return *this; 
 }
-
-
-// Operator de egalitate
 bool Player::operator==(const Player& other) const {
     return _symbol == other._symbol;
 }
